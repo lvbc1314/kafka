7d23fbbc73a976895dc62b36e3985fb2bb8a9da3
lvbingchuan <lvbc1314@qq.com>
2023-02-13 21:34:13 +0800

    111

--- src/main/java/lv/kafka/Controller/test.java
+++ src/main/java/lv/kafka/Controller/test.java
@@ -9,8 +9,6 @@ import java.util.ArrayList;
 public class test {
     public static void main(String[] args) {
         String ab="aghdfagf";
-        String ab1="aghdfagf";
-        System.out.println(1111);
         reg(ab);
     }
     private static Integer reg(String ab){
